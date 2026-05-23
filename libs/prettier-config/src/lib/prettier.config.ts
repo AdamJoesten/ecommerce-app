d9533bc5f5383cf002ca18f8be6bd479f53d66ff
Adam Joesten <adamjoesten@gmail.com>
2026-05-23 15:26:36 -0400

    feat: add prettier-config workspace package

---
+++ libs/prettier-config/src/lib/prettier.config.ts
@@ -0,0 +1,11 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import { type Config } from "prettier";
+
+export const baseConfig: Config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false
+};
+
