d2dd1a93a21f7502b10ed758f1c0a9f92d2f97d9
Adam Joesten <adamjoesten@gmail.com>
2026-05-23 15:26:40 -0400

    feat: enable prettier formatting in web and web-e2e apps

---
+++ apps/web-e2e/prettier.config.ts
@@ -0,0 +1,9 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import { type Config } from "prettier";
+import { baseConfig } from "@ecommerce/prettier-config"
+
+export const config: Config = {
+    ...baseConfig
+};
+
