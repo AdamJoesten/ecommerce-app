18536cd8f8c2410faacf2ba2e4519e7aaabc78bb
Adam Joesten <adamjoesten@gmail.com>
2026-05-23 15:26:48 -0400

    chore: silence no-require-imports for postcss.config.js

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { join } = require('path');
 
 // Note: If you use library-specific PostCSS/Tailwind configuration then you should remove the `postcssConfig` build
